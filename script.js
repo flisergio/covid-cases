8dce590f399a810c6bb3b151a38a97d33ba8e842
Sergio <sergey.dko@gmail.com>
2020-09-14 14:46:26 +0200

    Added files to deploy

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-async function getData() {
-    const response = await fetch("Nasa-zones.csv");
-    const data = await response.text();
-
-    const table = data.split('\n').slice(1);
-    table.forEach(row => {
-        const columns = row.split(',');
-        const year = columns[0];
-        const temp = columns[1];
-    });
-}
-
-getData();
-
 function vh(v) {
     var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     return (v * h) / 100;
